1d4cde6e725bc77c337d0a9d38411fa8ef350b7b
samb1232 <arterm212@gmail.com>
2025-07-24 13:55:40 +0500

    added pdd todo

--- lib/features/timer_screen/presentation/widgets/chosen_station_display.dart
+++ lib/features/timer_screen/presentation/widgets/chosen_station_display.dart
@@ -5,6 +5,7 @@ class ChosenStationDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // @todo #1: We should take and add actual values instead of placeholders
     return Column(
       children: [
         Text('Уралмаш', style: Theme.of(context).textTheme.headlineLarge),
